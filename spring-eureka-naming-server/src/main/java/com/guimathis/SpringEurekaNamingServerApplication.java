0c46a10928ca7a9968f3d49c4857af11a29db8ca
guimathias1906@gmail.com <guimathias1906@gmail.com>
2026-08-05 18:02:38 -0400

    Add Spring Eureka naming server to git

---
+++ spring-eureka-naming-server/src/main/java/com/guimathis/SpringEurekaNamingServerApplication.java
@@ -0,0 +1,15 @@
+package com.guimathis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringEurekaNamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEurekaNamingServerApplication.class, args);
+    }
+
+}
